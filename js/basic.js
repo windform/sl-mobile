d01b930fc4129d1048ff0f074c9160877ccda1cc
liaodong <867185661@qq.com>
2016-01-18 17:45:07 +0800

    20160118:sl-mobile

--- js/basic.js
+++ js/basic.js
@@ -1,14 +1,30 @@
 // JavaScript Document
-$(function(){
-	//初始化ajax请求
-	loadUrl('./templets/index.html');
-	
-	//单击底部选项卡发送对应的ajax请求
-	$("#bottom-tabs a").tap(function(){
-		$("#bottom-tabs a").removeClass('tab-active');
-		$(this).addClass('tab-active');
-		$('.loading').css('opacity',1);
-		loadUrl($(this).attr('src'));
-		return false;
-		});	
+$(function () {
+    //初始化ajax请求
+    loadUrl('./templets/index.html');
+
+    //单击底部选项卡发送对应的ajax请求
+    $("#bottom-tabs a").tap(function () {
+        $("#bottom-tabs a").removeClass('tab-active');
+        $(this).addClass('tab-active');
+        $('.loading').css('opacity', 1);
+        loadUrl($(this).attr('data-href'));
+        return false;
+    });
+
+
+    $("#airline_type a").live('tap', function () {
+        $("#airline_type a").removeClass('active');
+        $(this).addClass('active');
+        $(".type_tabs").css('display', 'none');
+        $(".type_tabs").eq($(this).index()).css('display', 'block');
+    });
+
+    $("#search").live('tap', function () {
+        loadSecondUrl('search_list.html');
+    })
+
+
+
+
 });
--- js/function.js
+++ js/function.js
@@ -7,4 +7,34 @@ function loadUrl(url){
 		  $('.sl_content').html(data)
 		  }
 	  })	
+};
+
+function loadSecondUrl(url){
+    $.ajax({
+        url: url,
+        type: 'get',
+        success: function (data) {
+            //$('.loading').css('opacity',0);
+            $('.main_body').html(data)
+        }
+    });
+    return false;
+}
+
+function exchange(){
+       $(".exchange").css({
+            left:($(".exchange").parent().width()-52)/2,
+            top:($(".exchange").parent().height()-47)/2
+      });
+
+}
+
+function headerBar(title,icon,right_font){
+    var style = '<div class="bar bar-header bar-dark"><a class="button icon-left ion-ios-arrow-back button-clear button-light"></a><h1 class="title">'+title+'</h1><a class="button icon-right '+icon+' button-clear button-light">'+right_font+'</a></div>';
+    $("body").append(style);
+}
+
+function subHeaderBar(left_icon,left_font,title,right_icon,right_font){
+    var style=' <div class="bar bar-subheader bar-light"><a class="button icon-left '+left_icon+' button-clear button-dark">'+left_font+'</a><h1 class="title">'+title+'</h1><a class="button icon-right '+right_icon+' button-clear button-dark">'+right_font+'</a></div>'
+     $("body").append(style);
 }
