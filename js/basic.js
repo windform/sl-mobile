6282bf92f026ff31c76954747127084ccbbccf3c
liaodong <867185661@qq.com>
2016-01-19 17:43:17 +0800

    20160119:sl-mobile

--- js/basic.js
+++ js/basic.js
@@ -1,17 +1,18 @@
 // JavaScript Document
 $(function () {
     //初始化ajax请求
-    loadUrl('./templets/index.html');
+    loadUrl('templets/index.html');
+	header('.frame_header','首页');
 
     //单击底部选项卡发送对应的ajax请求
     $("#bottom-tabs a").tap(function () {
         $("#bottom-tabs a").removeClass('tab-active');
         $(this).addClass('tab-active');
-        $('.loading').css('opacity', 1);
+        //$('.loading').css('opacity', 1);
+		header('.frame_header',$(this).text());
         loadUrl($(this).attr('data-href'));
         return false;
     });
-
 
     $("#airline_type a").live('tap', function () {
         $("#airline_type a").removeClass('active');
@@ -19,12 +20,19 @@ $(function () {
         $(".type_tabs").css('display', 'none');
         $(".type_tabs").eq($(this).index()).css('display', 'block');
     });
+	
+	exchange();
+	
+	$("#dtBox").DateTimePicker({ dateFormat: "yyyy-MM-dd" });
 
-    $("#search").live('tap', function () {
-        loadSecondUrl('search_list.html');
-    })
+	$("#search").live('tap',function(){
+		window.location.href='search_list.html';
+	});
 
-
+    $("#advance").live('tap',function(){
+        alert('hello');
+        window.location.href='advance.html';
+    });
 
 
 });
--- js/function.js
+++ js/function.js
@@ -4,7 +4,7 @@ function loadUrl(url){
 	  type:'get',
 	  success: function(data){
 		  $('.loading').css('opacity',0);
-		  $('.sl_content').html(data)
+		  $('.frame_content').html(data)
 		  }
 	  })	
 };
@@ -28,6 +28,10 @@ function exchange(){
       });
 
 }
+function header(ele,title){
+    var style = '<div class="bar bar-header bar-dark"><h1 class="title">'+title+'</h1></div>';
+    $(ele).html(style);
+}
 
 function headerBar(title,icon,right_font){
     var style = '<div class="bar bar-header bar-dark"><a class="button icon-left ion-ios-arrow-back button-clear button-light"></a><h1 class="title">'+title+'</h1><a class="button icon-right '+icon+' button-clear button-light">'+right_font+'</a></div>';
@@ -38,3 +42,15 @@ function subHeaderBar(left_icon,left_font,title,right_icon,right_font){
     var style=' <div class="bar bar-subheader bar-light"><a class="button icon-left '+left_icon+' button-clear button-dark">'+left_font+'</a><h1 class="title">'+title+'</h1><a class="button icon-right '+right_icon+' button-clear button-dark">'+right_font+'</a></div>'
      $("body").append(style);
 }
+
+function footerTabs(){
+	var html='<div class="tabs tabs-icon-top tabs-dark" id="bottom-tabs">'
+		+'<a class="tab-item tab-active" data-href="templets/index.html"><i class="icon ion-home"></i>首页</a>'
+		+'<a class="tab-item" data-href="templets/order.html"><i class="icon ion-document"></i>订单</a>'
+		+'<a class="tab-item" data-href="templets/action.html"><i class="icon ion-podium"></i>活动</a>'
+		+'<a class="tab-item" data-href="templets/service.html"><i class="icon ion-pinpoint"></i>服务</a>'
+		+'<a class="tab-item" data-href="templets/person.html" ><i class="icon ion-person"></i>我的</a>'
+	+'</div>';
+	$(".frame_footer").append(html);
+	
+	}
