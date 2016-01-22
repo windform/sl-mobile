d34a7eb95553f817a66477610b70504622a5ca16
liaodong <867185661@qq.com>
2016-01-22 18:00:36 +0800

    20160122:sl-mobile

--- js/basic.js
+++ js/basic.js
@@ -22,13 +22,31 @@ $(function () {
         $(".type_tabs").eq($(this).index()).css('display', 'block');
     });
 	
+
+    $("#search").live('tap',function(){
+        window.location.href='search_list.html';
+    });
+
 	exchange();
-	
 
+    //airline_detail.html
+    var list='<div class="list">';
+    for(var i=0; i<data.length; i++){
+        list+='<div class="item"><div class="row">'
+            +'<div class="col col-80">'
+            +'<div class="detail_shell"><span class="cabin">'+data[i].cabin+'</span><span class="price">'+data[i].price+'</span></div>'
+            +'<div class="detail_shell"><span class="seat">座位数:'+data[i].seat+'</span><span class="ticket_point">返点:'+data[i].ticket_point+'</span><span class="ticket_price">票面价:'+data[i].ticket_price+'</span></div>'
+            +'</div>'
+            +'<div class="col col-20"><button class="button button-assertive fr advance">预定</button></div>'
+            +'</div></div>'
+    }
+    list+='</div>';
+    $(".search_list_detail").html(list);
 
-	$("#search").live('tap',function(){
-		window.location.href='search_list.html';
-	});
+    $('.advance').live('tap',function(){
+         window.location.href='advance.html';
+    })
+
 
     //advance.html
     $("#next").live('tap',function(){   
@@ -70,7 +88,138 @@ $(function () {
         $('.row_hid').animate({opacity:0}, 300);
     })
 
+    //4、生成订单链接跳转
+    $('.order_button').live('tap',function(){
+        window.location.href='order_detail.html';
+    })
+
+
+
+    //order_detail.html
+    //1、单击按钮显示隐藏动画
+    $('.order_down').live('tap',function(){
+        var that=$(this);
+        $(this).css('display','none');
+        $(this).next().css('display','block');
+        $(this).parent().animate({height:110},300);
+        setTimeout(function(){
+             that.parent().find('.row_detail').css('display','flex');
+        },300)
+    });
+    $('.order_up').live('tap',function(){
+        var that=$(this);
+        $(this).css('display','none');
+        $(this).prev().css('display','block');
+        $(this).parent().animate({height:25,},300);
+        that.parent().find('.row_detail').css('display','none')
+    });
+
+    //2、单击按钮显示或隐藏退改签规定及政策说明
+    $('.order_read_btn').click(function(){
+        $('.row_order_rules').fadeToggle(300);
+        $(this).toggleClass('ion-arrow-up-b');
+
+    })
+
+    //3、机票信息数据渲染展示
+    var order_box='<div class="order_detail_list">';
+    for(var i=0;i<orderData.length;i++){
+        order_box+='<div class="row bor_bot3"><div class="col col-25"><p>'+orderData[i].name+'</p><p>身份证号</p><p>保险数量</p><p>票号</p></div><div class="col col-75"><p>'+orderData.person_type+'</p><p>610112198306184529</p><p>'orderData[i].ensure_num'</p><p>'+orderData[i].ticket_num+'</p></div></div><div class="row row_detail"><div class="col"><span>舱位:</span></div><div class="col"><span>'+orderData[i].cabin+'</span></div><div class="col"><span>票面结算价:</span></div><div class="col"><span class="red_color">'+orderData[i].ticket_price+'</span></div></div><div class="row row_detail"><div class="col"><span>机建/燃油:</span></div><div class="col"><span>'+orderData[i].airline_taste+'</span></div><div class="col"><span>单张票价:</span></div><div class="col"><span class="red_color">'+orderData[i].single_price+'</span></div></div><div class="order_toggle"><div class="row row_detail"><div class="col"><span>返点:</span></div><div class="col"><span>'+orderData[i].ticket_point+'</span></div><div class="col"><span>单张代理费:</span></div><div class="col"><span class="red_color">'+orderData[i].agent_price+'</span></div></div><div class="row row_detail"><div class="col"><span>机票推荐:</span></div><div class="col"><span class="policy_type">'+orderData[i].ticketout_recommend+'</span></div><div class="col"><span>政策类型:</span></div><div class="col"><span style="width:80px">'+orderData[i].policy_type+'</span></div></div><div class="row row_detail"><div class="col"><span>出票时间:</span></div><div class="col"><span style="width:100px">'+orderData[i].ticketout_time+'</span></div><div class="col"></div><div class="col"></div></div><a class="button ion-chevron-down order_toggle_btn order_down"></a><a class="button ion-chevron-up order_toggle_btn order_up"></a></div>'
+    }
+
+    order_box+='</div>';
+    $('.order_detail_wrap').html(order_box)     
+
 });
+
+
+
+
+
+
+
+
+//订单详细虚假模拟数据
+var orderData=[{
+    'name':'张三',
+    'person_num':'36078216457828',
+    'person_type':'成人',
+    'ensure_num':'8',
+    'ticket_num':'Yu7894667',
+    'cabin':'R',
+    'ticket_price':'$542.52',
+    'airline_taste':'￥50/0',
+    'single_price':'￥591.52',
+    'ticket_point':'3.3%',
+    'agent_price':'￥18.48',
+    'ticketout_recommend':'标准政策',
+    'policy_type':'B2B,PSB',
+    'ticketout_time':'00:00-23:59'
+},{
+    'name':'小李',
+    'person_num':'465434687654674',
+    'person_type':'成人',
+    'ensure_num':'2',
+    'ticket_num':'TR78915567',
+    'cabin':'经济舱',
+    'ticket_price':'$1022.52',
+    'airline_taste':'￥50/0',
+    'single_price':'￥1201.52',
+    'ticket_point':'5.3%',
+    'agent_price':'￥50.48',
+    'ticketout_recommend':'紧急政策',
+    'policy_type':'B2C',
+    'ticketout_time':'07:00-23:59'
+},{
+    'name':'王小二',
+    'person_num':'JHFB87654674',
+    'person_type':'儿童',
+    'ensure_num':'４',
+    'ticket_num':'NG78915567',
+    'cabin':'无座',
+    'ticket_price':'$200.62',
+    'airline_taste':'￥50/0',
+    'single_price':'￥301.52',
+    'ticket_point':'1.3%',
+    'agent_price':'￥19.48',
+    'ticketout_recommend':'标准政策',
+    'policy_type':'B2B,B2G',
+    'ticketout_time':'07:00-18:59'
+}]
+
+
+//预定舱位虚假模拟数据
+var data=[{
+    "price":"￥541.52",
+    "ticket_point":"3.3%",
+    "ticket_price":"￥560",
+    "seat":">9",
+    "cabin":"45折R舱"
+},{
+    "price":"￥571.52",
+    "ticket_point":"3.0%",
+    "ticket_price":"￥580",
+    "seat":">3",
+    "cabin":"45折R舱"
+},{
+    "price":"￥300.52",
+    "ticket_point":"1.2%",
+    "ticket_price":"￥800",
+    "seat":">5",
+    "cabin":"45折R舱"
+},{
+    "price":"￥1800.52",
+    "ticket_point":"5.0%",
+    "ticket_price":"￥800",
+    "seat":">6",
+    "cabin":"头等舱"
+},{
+    "price":"￥900.00",
+    "ticket_point":"4.2%",
+    "ticket_price":"￥1200",
+    "seat":">8",
+    "cabin":"78折C舱"
+}]
 
 
 //政策选择虚假模拟数据
@@ -103,3 +252,6 @@ var policydata=[{
     'policy_remarks':'政策备注：换编码出票！此政策只适用于高价，低价不享受，否则拒单，改期、升舱、换开、签转外航以及政策航班改到无政策航班，需收回代理费需要换编码出票，外放无舱位请勿导入，否则拒单，此政策只适用于高价，低价不享受，否则拒单，改期、升舱、换开、签转外航以及政策航班改到无政策航班，需收回代理费'
 
 },]
+
+
+
